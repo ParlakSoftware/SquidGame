b567ecfac6d97d63d99d3344c080c69313d0887a
Parlak Software <parlaksoft01@gmail.com>
2021-10-19 18:57:06 +0300

    Animasyonlar Eklendi

--- Assets/Scripts/Class/Character.cs
+++ Assets/Scripts/Class/Character.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Class;
+using Assets.Scripts.Interface;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,8 +10,7 @@ public class Character : MonoBehaviour, ICharacter
     private string name;
     private bool stop;
     private Rigidbody rigidbody;
-    public Color renk { get; set; }
-
+    private Animator anim;
     public string Name
     {
         get
@@ -40,27 +41,35 @@ public class Character : MonoBehaviour, ICharacter
         set { this.stop = value; }
     }
 
-    public Character(string name, float speed, Rigidbody rigidbody)
+    public Character(string name, float speed, Rigidbody rigidbody,Animator anim)
     {
         this.Name = name;
         this.Speed = speed;
         this.rigidbody = rigidbody;
+        this.anim = anim;
+        isStop = true;
     }
 
     public void Dead()
     {
         this.Name = "Öldüm";
-        Debug.Log(Name);
+        Speed = 0;
+        anim.SetBool("Dead", true);
     }
 
     public void Idle()
     {
         isStop = true;
+        anim.SetBool("Idle", true);
+        anim.SetBool("Walk", false);
+        rigidbody.velocity = Vector3.zero;
     }
 
     public void Walk()
     {
         rigidbody.velocity = new Vector3(0, 0, Speed);
+        anim.SetBool("Idle", false);
+        anim.SetBool("Walk", true);
         isStop = false;
     }
 }
--- Assets/Scripts/Manager/CharacterManager.cs
+++ Assets/Scripts/Manager/CharacterManager.cs
@@ -1,4 +1,5 @@
-﻿using Assets.Scripts.Class.DeviceController;
+﻿using Assets.Scripts.Class;
+using Assets.Scripts.Class.DeviceController;
 using Assets.Scripts.Interface;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,13 +11,13 @@ public class CharacterManager : MonoBehaviour
     public float speed;
     public bool stop;
     public ICharacter character;
+
     IDeviceController deviceController;
     // Start is called before the first frame update
     void Start()
     {
-        character = new Character(name, speed, GetComponent<Rigidbody>());
+        character = new Character(name, speed, GetComponent<Rigidbody>(),GetComponent<Animator>());
         deviceController = new DesktopController();
-        GetComponent<MeshRenderer>().material.color = Color.blue;
     }
 
     // Update is called once per frame
